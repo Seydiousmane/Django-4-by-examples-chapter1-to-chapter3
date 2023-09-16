1ae737498cd9b6f2f85483cda7f642b7c7b23104
Seydina Ousmane Diatta <diattaseydi92@gmail.com>
2023-09-16 01:44:22 +0000

    TrigramSimilarity search

--- blogging/urls.py
+++ blogging/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('tag/<slug:tag_slug>', views.post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
-    path('<int:post_id>/share/', views.post_share, name='post_share')
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('search', views.post_search_with_trigram_similarity, name='post_search')
 ]
--- blogging/views.py
+++ blogging/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchRank, SearchQuery, SearchVector, TrigramSimilarity
+
 # Create your views here.
 
 
@@ -80,3 +82,81 @@ def post_share(request, post_id):
         form = EmailPostForm()
     return render(request, 'blogging/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
+
+def post_search(request):
+    form = SearchForm
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search = SearchVector('title', 'body')
+            ).filter(search=query)
+
+    context = {'form':form, 'query': query, 'results': results}
+    return render(request, 'blogging/post/search.html', context)
+
+
+#Post search Method steeming with SeachVector, SearchQuery, SearchRank
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+
+    context = {'form':form, 'query': query, 'results': results}
+    return render(request, 'blogging/post/search.html', context)
+
+
+#Post search Method steeming with SeachVector, SearchQuery, SearchRank with weight (A=1, D=0.1, C=0.2, B=0.1)
+def post_search(request):
+    form = SearchForm
+    query = None
+    results = SearchForm()
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + \
+                            SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('rank')
+
+    context = {'form':form, 'query': query, 'results': results}
+    return render(request, 'blogging/post/search.html', context)
+
+
+#Search with trigram similarity
+def post_search_with_trigram_similarity(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+
+    return render(request, 'blogging/post/search.html', {'form':form, 'query': query, 'results': results})
+
+    
